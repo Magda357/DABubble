780ad70e7aefea8ab3146852e15d5d3c3a86bebf
Magda Danielyan <mdanielyan50@gmail.com>
2025-10-14 16:15:28 +0200

    svg icons hover

--- src/app/shared/shared.component.ts
+++ src/app/shared/shared.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-shared',
-  imports: [],
+  imports: [NgIf],
+  standalone: true,
   templateUrl: './shared.component.html',
   styleUrl: './shared.component.scss'
 })
 export class SharedComponent {
+isWorkspaceOpen = false;
 
+toggleWorkspace() {
+  this.isWorkspaceOpen = !this.isWorkspaceOpen;
+}
 }

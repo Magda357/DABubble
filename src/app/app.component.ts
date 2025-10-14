3ff67285911f21d49b133b52c67c5746af00d988
Magda Danielyan <mdanielyan50@gmail.com>
2025-10-14 02:24:35 +0200

    added shared component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import{HeaderComponent } from './main-content/header/header.component'
+import{HeaderComponent } from './main-content/header/header.component';
+import { SharedComponent } from './shared/shared.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent,SharedComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

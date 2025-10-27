84d7a6a615589705e83e1dbc5c37c9f15e1ce6bc
Magda Danielyan <mdanielyan50@gmail.com>
2025-10-27 11:50:08 +0100

    user burger menu created

--- src/app/main-content/header/header.component.ts
+++ src/app/main-content/header/header.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
-
+import { CommonModule, NgIf } from '@angular/common';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgIf, CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  showUserMenu = false;
 
+  toggleUserMenu() {
+    this.showUserMenu = !this.showUserMenu;
+    if (this.showUserMenu) {
+      document.body.classList.add('user-menu-open');
+    } else {
+      document.body.classList.remove('user-menu-open');
+    }
+  }
+
+  closeUserMenu() {
+    this.showUserMenu = false;
+    document.body.classList.remove('user-menu-open');
+  }
+  openProfile() {
+    // Logik zum Öffnen des Profils
+    console.log('Profil geöffnet');
+    this.showUserMenu = false; // Menü schließen
+  }
+
+  logOut() {
+    this.showUserMenu = false; // Menü schließen
+    // Logik zum Ausloggen
+    console.log('Ausgeloggt');
+  }
 }

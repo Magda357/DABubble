fe456ab8c86e843d9539fa8acce6b615308ff9c1
Magda Danielyan <mdanielyan50@gmail.com>
2025-10-27 12:18:57 +0100

    splash animation created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { SplashAnimationComponent } from './splash-animation/splash-animation.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'splash', component: SplashAnimationComponent },
+];
---
+++ src/app/splash-animation/splash-animation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-splash-animation',
+  imports: [],
+  templateUrl: './splash-animation.component.html',
+  styleUrl: './splash-animation.component.scss'
+})
+export class SplashAnimationComponent {
+
+}

68588dceb1551f19b2f359dafebc1710becfc971
Gaetano-Leanza <gaetano1981@live.de>
2025-12-25 00:47:28 +0100

    work on auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { StartAnimationComponent } from './auth/start-animation/start-animation.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { SelectAvatarComponent } from './auth/select-avatar/select-avatar.component';
+import { DataService } from './shared/services/data.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    LoginComponent,
+    MainContentComponent,
+    SignUpComponent,
+    StartAnimationComponent,
+    SelectAvatarComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'dabubble-app';
+  constructor(private router: Router, private dataService: DataService) {}
+  ngOnInit() {
+    this.dataService.loadCurrentUser();
+  }
 }
